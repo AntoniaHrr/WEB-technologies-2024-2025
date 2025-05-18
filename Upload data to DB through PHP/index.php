9233e92066f55fa11544be93e33b2cd1f6662e09
Antonia Hristova <antoniya1245@gmail.com>
2025-05-18 23:38:54 +0300

    Create index.php

---
+++ Upload data to DB through PHP/index.php
@@ -0,0 +1,17 @@
+    <?php
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <form action="./includes/register.php" method="POST">
+        <input type="text" name="username">
+        <input type="text" name="email">
+        <input type="password" name="pwd">
+        <button>Submit</button>
+        </form>
+    </body>
+    </html>
